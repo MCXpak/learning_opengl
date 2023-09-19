3d6ca0024d8a1841677a4afd3112721a1ce5f0ac
MCXpak <mouradlasga@gamil.com>
2023-09-19 22:58:35 +0100

    add 2 textures, also awesome face is facing opposite direction

--- learning_opengl/learning_opengl/Source.cpp
+++ learning_opengl/learning_opengl/Source.cpp
@@ -2,6 +2,8 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include "Shader_s.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
@@ -31,96 +33,88 @@ int main()
         return -1;
     }
 
-    float triangle[] = {
-        0.0f, 0.5f, 0.0f,
-        0.5f, 0.0f, 0.0f,
-        -0.5f, 0.0f, 0.0f
-    };
-
-    float vertices[] = {
-        // positions         // colors
-         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
-        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
-         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
+    float rectangle[] = {
+        // positions          // colors           // texture coords
+         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
     };
 
     unsigned int indices[] = {  // note that we start from 0!
-        0, 1, 3,   // first triangle
-        1, 2, 3    // second triangle
+    0, 1, 3,   // first triangle
+    1, 2, 3    // second triangle
     };
-
-    float twoTriangles[] = {
-        -0.25f, 0.25f, 0.0f,
-        -0.5f, 0.0f, 0.0f,
-        0.0f, 0.0f, 0.0f,
-        0.25f, 0.25f, 0.0f,
-        0.5f, 0.0f, 0.0f,
-    };
-
-    float triangleOne[] = {
-        0.25f, 0.25f, 0.0f,
-        0.5f, 0.0f, 0.0f,
-        0.0f, 0.0f, 0.0f
-    };
-
-    float triangleTwo[] = {
-        -0.25f, 0.25f, 0.0f,
-        0.0f, 0.0f, 0.0f,
-        -0.5f, 0.0f, 0.0f
-    };
-
-    unsigned int twoTrianglesIndices[] = {
-        3, 4, 2,
-        0, 2, 1
-    };
-
-    //Creating 2 trianles
-    // Can create 2 unsigned int VAOs/VBOs or use
-    // unsigned int VAOs[2], VBOs[2] as well
 
     //First Triangle
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
-
     glBindVertexArray(VAO);
 
     unsigned int VBO;
     glGenBuffers(1, &VBO);
-
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    //unsigned int EBO;
-    //glGenBuffers(1, &EBO);
-
-    //unsigned int EBO_TwoTriangles;
-    //glGenBuffers(1, &EBO_TwoTriangles);
-
-    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(twoTrianglesIndices), twoTrianglesIndices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(rectangle), rectangle, GL_STATIC_DRAW);
+    
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
     // color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
-    //Second Triangle
-    //unsigned int VAO_TriangleTwo;
-    //glGenVertexArrays(1, &VAO_TriangleTwo);
+    // texture attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    //generate and bind texture object
+    unsigned int texture1;
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+
+    // set the texture wrapping/filtering options (on the currently bound texture object)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    //load and generate texture
+
+    stbi_set_flip_vertically_on_load(true);
+
+    int width, height, nrChannels;
+    unsigned char* data1 = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+    if (data1)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data1);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+
+    stbi_image_free(data1); //free image memory
+
+    unsigned int texture2;
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+
+    unsigned char* data2 = stbi_load("awesomeface.png", &width, &height, &nrChannels, 0);
+    if (data2)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data2);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+
+    stbi_image_free(data2); //free image memory
     
-    //glBindVertexArray(VAO_TriangleTwo);
-
-    //unsigned int VBO_TriangleTwo;
-    //glGenBuffers(1, &VBO_TriangleTwo);
-
-    //glBindBuffer(GL_ARRAY_BUFFER, VBO_TriangleTwo);
-    //glBufferData(GL_ARRAY_BUFFER, sizeof(triangleTwo), triangleTwo, GL_STATIC_DRAW);
-
-    //glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    //glEnableVertexAttribArray(0);
 
     Shader ourShader("./shader.vs", "./shader.fs");
 
@@ -137,6 +131,8 @@ int main()
 
         //activate shader
         ourShader.use();
+        ourShader.setInt("texture1", 0);
+        ourShader.setInt("texture2", 1);
         //ourShader.setFloat("offset", 0.5);
         // update the uniform color
         //float timeValue = glfwGetTime();
@@ -144,9 +140,13 @@ int main()
         //int vertexColorLocation = glGetUniformLocation(shaderProgramT1, "ourColor");
         //glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 
-        
+        glActiveTexture(GL_TEXTURE0); // activate the texture unit first before binding texture
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1); // activate the texture unit first before binding texture
+        glBindTexture(GL_TEXTURE_2D, texture2);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
         //glUseProgram(shaderProgramT2);
         //glBindVertexArray(VAO_TriangleTwo);
         //glDrawArrays(GL_TRIANGLES, 0, 3);
--- learning_opengl/learning_opengl/shader.vs
+++ learning_opengl/learning_opengl/shader.vs
@@ -1,10 +1,13 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
+layout (location = 2) in vec2 aTexCoord;
+
 out vec3 ourColor;
-uniform float offset = 0;
+out vec2 TexCoord;
+
 void main(){
-   vec3 aPosPlusOffset = vec3(aPos.x + offset, aPos.y, aPos.z);
-   gl_Position = vec4(aPosPlusOffset, 1.0);
-   ourColor = vec3(aColor);
+   gl_Position = vec4(aPos, 1.0);
+   ourColor = aColor;
+   TexCoord = vec2(aTexCoord.x, aTexCoord.y);
 }
